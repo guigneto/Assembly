d512b88a772fade78b4b39f862128424d6f42780
Guilherme Gomes <guilhermeneto26@hotmail.com>
2024-06-24 17:44:09 -0300

    feat: Refatorar programa C para verificar se um número é primo

--- trab-assembly/problema2.c
+++ trab-assembly/problema2.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    int num, i, count = 0;
+    int num, count = 0;
     int divisores[100]; 
 
     printf("Digite um numero: ");
     scanf("%d", &num);
 
-    // Verifica divisores de 2 até num/2
-    for (i = 2; i <= num / 2; i++) {
+    for (int i=2;i<=num/2;i++) {
         if (num % i == 0) {
-            divisores[count++] = i; // Armazena o divisor e incrementa o contador
+            divisores[count++] = i;
         }
     }
 
-    if (count == 0) { // Se count é 0, então num é primo
+    if (count == 0) { 
         printf("%d eh primo\n", num);
     } else {
         printf("%d nao eh primo e tem como divisores ", num);
-        for (i = 0; i < count; i++) { // Imprime todos os divisores encontrados
+        for (int i=0;i<count;i++) { 
             printf("%d ", divisores[i]);
         }
         printf("\n");
