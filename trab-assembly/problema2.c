cef90898dd68a7425acd99f42ab0a730b39f4649
Guilherme Gomes <guilhermeneto26@hotmail.com>
2024-06-24 17:27:46 -0300

    feat: Adicionar loop para contar até o número digitado pelo usuário

---
+++ trab-assembly/problema2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void){
+
+    int num,count;
+
+    printf("Digite um numero: ");
+    scanf("%d",&num);
+
+    while(count<num){
+        
+    }
+
+    return 0;
+}
